20ead3a76780542031bdbc523f846170a86c12c7
coolAlias <BASbliznet@gmail.com>
2014-03-20 15:50:30 +0800

    Cleaned up code

--- src/main/java/zeldaswordskills/entity/projectile/EntityLeapingBlow.java
+++ src/main/java/zeldaswordskills/entity/projectile/EntityLeapingBlow.java
@@ -38,21 +38,21 @@ public class EntityLeapingBlow extends EntityThrowable
 {
 	/** Keeps track of entities already affected so they don't get attacked twice */
 	private List<Integer> struckEntities = new ArrayList<Integer>(); 
-	
+
 	/** Base damage should be set from player's Leaping Blow skill */
 	private float damage = 2.0F;
-	
+
 	/** Base number of ticks this entity can exist */
 	private int lifespan = 12;
-	
+
 	/** Skill level of swordsman; used in many calculations */
 	private int level = 0;
-	
+
 	/** Whether the swordsman is wielding the master sword */
 	private boolean isMaster = false;
-	
+
 	private static final float BASE_SIZE = 1.0F, HEIGHT = 0.5F;
-	
+
 	public EntityLeapingBlow(World world) {
 		super(world);
 		this.setSize(BASE_SIZE, HEIGHT);
@@ -70,7 +70,7 @@ public class EntityLeapingBlow extends EntityThrowable
 		super(world, x, y, z);
 		this.setSize(BASE_SIZE, HEIGHT);
 	}
-	
+
 	/**
 	 * Each level increases the distance traveled as well as the AoE
 	 * Master version increases weakness effect duration
@@ -81,7 +81,7 @@ public class EntityLeapingBlow extends EntityThrowable
 		this.isMaster = isMaster;
 		return this;
 	}
-	
+
 	/**
 	 * Sets amount of damage that will be caused onImpact
 	 */
@@ -89,51 +89,48 @@ public class EntityLeapingBlow extends EntityThrowable
 		this.damage = amount;
 		return this;
 	}
-	
+
 	/** Max distance (squared) from thrower that damage can still be applied */
 	private double getRangeSquared() { return (3.0D + level) * (3.0D + level); }
-	
+
 	/** Duration of weakness effect */
 	private int getPotionDuration() { return ((isMaster ? 110 : 50) + (level * 10)); }
-	
+
 	/** Returns area within which to search for targets each tick */
 	private AxisAlignedBB getAoE() { return boundingBox.expand((0.25F * level), 0.0F, (0.25F * level)); }
 
 	@Override
 	public void onUpdate() {
 		super.onUpdate();
-		if (inGround || ticksExisted > lifespan) { setDead(); }
-		
+		if (inGround || ticksExisted > lifespan) {
+			setDead();
+		}
 		if (!worldObj.isRemote) {
 			List<EntityLivingBase> targets = worldObj.getEntitiesWithinAABB(EntityLivingBase.class, getAoE());
 			for (EntityLivingBase target : targets) {
 				if (!struckEntities.contains(target.entityId) && target != getThrower() && !TargetUtils.isTargetInFrontOf(this, target, 30F)) {
 					struckEntities.add(target.entityId);
 					float d = damage;
-					
 					if (getThrower() != null) {
 						double d0 = (1.0D - getThrower().getDistanceSqToEntity(target) / getRangeSquared());
 						d *= (d0 > 1.0D ? 1.0D : d0);
 						if (d < 0.5D) { return; }
 					}
-					//System.out.println("Leaping blow impacted; damage: " + d + "; is master? " + isMaster);
 					if (target.attackEntityFrom(DamageUtils.causeIndirectSwordDamage(this, getThrower()), d)) {
 						target.addPotionEffect(new PotionEffect(Potion.weakness.id, getPotionDuration()));
 					}
 				}
 			}
 		}
-		
+
 		/** Velocity x and z for spawning particles to left and right of entity */
 		double vX = motionZ;
 		double vZ = motionX;
-		
 		String particle = (isMaster ? "magicCrit" : "crit");
 		int blockID = worldObj.getBlockId((int) (posX + (boundingBox.maxX - boundingBox.minX) / 2), (int) posY - 1, (int) (posZ + (boundingBox.maxZ - boundingBox.minZ) / 2));
 		if (blockID > 0) {
 			particle = "tilecrack_" + blockID + "_" + worldObj.getBlockMetadata((int) (posX + (boundingBox.maxX - boundingBox.minX) / 2), (int) posY - 1, (int) (posZ + (boundingBox.maxZ - boundingBox.minZ) / 2));
 		}
-		
 		for (int i = 0; i < 4; ++i) {
 			worldObj.spawnParticle(particle, posX, posY, posZ, vX + rand.nextGaussian(), 0.01D, vZ + rand.nextGaussian());
 			worldObj.spawnParticle(particle, posX, posY, posZ, -vX + rand.nextGaussian(), 0.01D, -vZ + rand.nextGaussian());
@@ -147,9 +144,7 @@ public class EntityLeapingBlow extends EntityThrowable
 				Entity entity = mop.entityHit;
 				if (entity instanceof EntityLivingBase && !struckEntities.contains(entity.entityId) && entity != getThrower()) {
 					struckEntities.add(entity.entityId);
-					//System.out.println("Leaping blow impacted; damage: " + damage + "; is master? " + isMaster);
 					if (entity.attackEntityFrom(DamageUtils.causeIndirectSwordDamage(this, getThrower()), damage)) {
-						// TODO in 1.7, "damage.hit" becomes "damage.hurtflesh" OR make own hit sound
 						entity.worldObj.playSoundAtEntity(entity, "damage.hit", (entity.worldObj.rand.nextFloat() * 0.4F + 0.5F), 1.0F / (entity.worldObj.rand.nextFloat() * 0.4F + 0.5F));
 						if (entity instanceof EntityLivingBase) {
 							((EntityLivingBase) entity).addPotionEffect(new PotionEffect(Potion.weakness.id, 60));
@@ -171,7 +166,7 @@ public class EntityLeapingBlow extends EntityThrowable
 
 	@Override
 	public float getGravityVelocity() { return 0.0F; }
-	
+
 	@Override
 	public void writeEntityToNBT(NBTTagCompound compound) {
 		super.writeEntityToNBT(compound);
@@ -180,7 +175,7 @@ public class EntityLeapingBlow extends EntityThrowable
 		compound.setInteger("level", level);
 		compound.setInteger("lifespan", lifespan);
 	}
-	
+
 	@Override
 	public void readEntityFromNBT(NBTTagCompound compound) {
 		super.readEntityFromNBT(compound);
