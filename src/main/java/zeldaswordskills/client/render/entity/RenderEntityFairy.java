f05d041127ffb4047ac1b7a34a64bec1b62c7302
coolAlias <BASbliznet@gmail.com>
2014-03-21 17:48:00 +0800

    Fairy texture no longer uses vanilla resource path

--- src/main/java/zeldaswordskills/client/render/entity/RenderEntityFairy.java
+++ src/main/java/zeldaswordskills/client/render/entity/RenderEntityFairy.java
@@ -28,14 +28,15 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
 import zeldaswordskills.entity.EntityFairy;
+import zeldaswordskills.lib.ModInfo;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
 public class RenderEntityFairy extends Render
 {
-	/** Use XP Orb texture for now */
-	private static final ResourceLocation texture = new ResourceLocation("textures/entity/experience_orb.png");
+	/** Copy of XP Orb texture */
+	private static final ResourceLocation texture = new ResourceLocation(ModInfo.ID, "textures/entity/fairy.png");
 
 	public RenderEntityFairy() {
 		shadowSize = 0.15F;
@@ -44,39 +45,39 @@ public class RenderEntityFairy extends Render
 	
 	public void renderFairy(EntityFairy fairy, double x, double y, double z, float yaw, float partialTick) {
 		GL11.glPushMatrix();
-        GL11.glTranslatef((float)x, (float)y, (float)z);
+        GL11.glTranslated(x, y, z);
         bindEntityTexture(fairy);
         int i = 10;
-        float f2 = (float)(i % 4 * 16 + 0) / 64.0F;
-        float f3 = (float)(i % 4 * 16 + 16) / 64.0F;
-        float f4 = (float)(i / 4 * 16 + 0) / 64.0F;
-        float f5 = (float)(i / 4 * 16 + 16) / 64.0F;
+        float f2 = (i % 4 * 16 + 0) / 64.0F;
+        float f3 = (i % 4 * 16 + 16) / 64.0F;
+        float f4 = (i / 4 * 16 + 0) / 64.0F;
+        float f5 = (i / 4 * 16 + 16) / 64.0F;
         float f6 = 1.0F;
         float f7 = 0.5F;
         float f8 = 0.25F;
         int j = fairy.getBrightnessForRender(partialTick);
         int k = j % 65536;
         int l = j / 65536;
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)k / 1.0F, (float)l / 1.0F);
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, k / 1.0F, l / 1.0F);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         float f9 = 255.0F;
-        float f10 = ((float) 0x00EE00 + partialTick + fairy.ticksExisted) / 2.0F;
+        float f10 = (0x00EE00 + partialTick + fairy.ticksExisted) / 2.0F;
         l = (int)((MathHelper.sin(f10) + 1.0F) * 0.5F * f9);
         int i1 = (int)f9;
         int j1 = (int)((MathHelper.sin(f10 + 4.1887903F) + 1.0F) * 0.1F * f9);
         int k1 = l << 16 | i1 << 8 | j1;
-        GL11.glRotatef(180.0F - this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(-this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+        GL11.glRotatef(180.0F - renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(-renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
         float f11 = 0.3F;
         GL11.glScalef(f11, f11, f11);
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
         tessellator.setColorRGBA_I(k1, 128);
         tessellator.setNormal(0.0F, 1.0F, 0.0F);
-        tessellator.addVertexWithUV((double)(0.0F - f7), (double)(0.0F - f8), 0.0D, (double)f2, (double)f5);
-        tessellator.addVertexWithUV((double)(f6 - f7), (double)(0.0F - f8), 0.0D, (double)f3, (double)f5);
-        tessellator.addVertexWithUV((double)(f6 - f7), (double)(1.0F - f8), 0.0D, (double)f3, (double)f4);
-        tessellator.addVertexWithUV((double)(0.0F - f7), (double)(1.0F - f8), 0.0D, (double)f2, (double)f4);
+        tessellator.addVertexWithUV((0.0F - f7), (0.0F - f8), 0.0D, f2, f5);
+        tessellator.addVertexWithUV((f6 - f7), (0.0F - f8), 0.0D, f3, f5);
+        tessellator.addVertexWithUV((f6 - f7), (1.0F - f8), 0.0D, f3, f4);
+        tessellator.addVertexWithUV((0.0F - f7), (1.0F - f8), 0.0D, f2, f4);
         tessellator.draw();
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glDisable(GL12.GL_RESCALE_NORMAL);
