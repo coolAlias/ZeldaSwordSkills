f10460386c90e2a81a62a9d58cbaf54a65b6e376
coolAlias <BASbliznet@gmail.com>
2014-08-26 19:30:56 -0700

    Logger can now be set to show FINE+ messages

--- src/main/java/zeldaswordskills/util/LogHelper.java
+++ src/main/java/zeldaswordskills/util/LogHelper.java
@@ -17,20 +17,84 @@
 
 package zeldaswordskills.util;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 import cpw.mods.fml.common.FMLLog;
 
 public class LogHelper
 {
-	private static Logger logger = Logger.getLogger("ZELDA SWORD SKILLS");
-	
-	public static void init() {
-		logger.setParent(FMLLog.getLogger());
+	private static Logger logger = Logger.getLogger("ZeldaSwordSkills");
+	private static Handler consoleHandler = new ConsoleHandler();
+	private static Formatter zssFormatter = new ZSSLogFormatter();
+
+	/**
+	 * Sets up the logger to output messages of up to the level given
+	 */
+	public static void init(Level level) {
+		logger.setLevel(level);
+		// only use custom handler when necessary to output FINE or lower logs
+		if (level.intValue() < Level.INFO.intValue()) { 
+			consoleHandler.setFormatter(zssFormatter);
+			consoleHandler.setLevel(logger.getLevel());
+			logger.addHandler(consoleHandler);
+		} else { // otherwise use FML logger formatting
+			logger.setParent(FMLLog.getLogger());
+		}
 	}
-	
+
 	public static void log(Level logLevel, String message) {
 		logger.log(logLevel, message);
+	}
+}
+
+/**
+ * 
+ * Copied from cpw.mods.fml.relauncher.FMLLogFormatter
+ *
+ */
+final class ZSSLogFormatter extends Formatter {
+	static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	//private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	public String format(LogRecord record)
+	{
+		StringBuilder msg = new StringBuilder();
+		//msg.append(this.dateFormat.format(Long.valueOf(record.getMillis())));
+		Level lvl = record.getLevel();
+
+		String name = lvl.getLocalizedName();
+		if ( name == null ) {
+			name = lvl.getName();
+		}
+
+		if ((name != null) && (name.length() > 0)) {
+			msg.append("[" + name + "] ");
+		} else {
+			msg.append(" ");
+		}
+
+		if (record.getLoggerName() != null) {
+			msg.append("["+record.getLoggerName()+"] ");
+		} else {
+			msg.append("[] ");
+		}
+		msg.append(formatMessage(record));
+		msg.append(LINE_SEPARATOR);
+		Throwable thr = record.getThrown();
+
+		if (thr != null) {
+			StringWriter thrDump = new StringWriter();
+			thr.printStackTrace(new PrintWriter(thrDump));
+			msg.append(thrDump.toString());
+		}
+
+		return msg.toString();
 	}
 }
