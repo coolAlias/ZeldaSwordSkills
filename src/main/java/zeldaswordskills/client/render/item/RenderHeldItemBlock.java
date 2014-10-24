9f1dc271db5af419555dc525c1c73210907030ba
coolAlias <BASbliznet@gmail.com>
2014-10-24 07:23:55 -0700

    Fix crash picking up certain blocks with gauntlets

--- src/main/java/zeldaswordskills/client/render/item/RenderHeldItemBlock.java
+++ src/main/java/zeldaswordskills/client/render/item/RenderHeldItemBlock.java
@@ -60,11 +60,6 @@ public class RenderHeldItemBlock implements IItemRenderer
 					GL11.glScalef(1.0F, 1.0F, -1.0F);
 					GL11.glRotatef(210.0F, 1.0F, 0.0F, 0.0F);
 					GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-					int i1 = Item.getItemFromBlock(block).getColorFromItemStack(stack, 0);
-					float f = (float)(i1 >> 16 & 255) / 255.0F;
-					float f1 = (float)(i1 >> 8 & 255) / 255.0F;
-					float f2 = (float)(i1 & 255) / 255.0F;
-					GL11.glColor4f(f, f1, f2, 1.0F);
 					GL11.glRotatef(-90.0F, 0.0F, 1.0F, 0.0F);
 					blockRenderer.renderBlockAsItem(block, ((ItemHeldBlock) item).getMetaFromStack(stack), 1.0F);
 					GL11.glPopMatrix();
