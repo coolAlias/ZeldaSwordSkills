d85a6807f01fff01247cb4d07a564cfe2e4087e9
coolAlias <BASbliznet@gmail.com>
2015-01-19 18:53:29 -0800

    Updated version

--- src/main/java/zeldaswordskills/ref/ModInfo.java
+++ src/main/java/zeldaswordskills/ref/ModInfo.java
@@ -20,7 +20,7 @@ package zeldaswordskills.ref;
 public class ModInfo {
 	public static final String ID = "zeldaswordskills";
 	public static final String NAME = "Zelda Sword Skills";
-	public static final String VERSION = "1.7.10-0.2.0";
+	public static final String VERSION = "1.7.10-0.2.1";
 	public static final String CHANNEL = "zss_channel";
 	public static final String CLIENT_PROXY = "zeldaswordskills.ClientProxy";
 	public static final String COMMON_PROXY = "zeldaswordskills.CommonProxy";
