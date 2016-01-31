f55952debddea65f8ae5342c0d9feb30299182ba
coolAlias <BASbliznet@gmail.com>
2016-01-30 21:50:27 -0800

    Use font height instead of magic number

--- src/main/java/zeldaswordskills/client/gui/ComboOverlay.java
+++ src/main/java/zeldaswordskills/client/gui/ComboOverlay.java
@@ -121,7 +121,7 @@ public class ComboOverlay extends Gui implements IGuiOverlay
 					mc.fontRendererObj.drawString(StatCollector.translateToLocal("combo.damage") + ": " + String.format("%.1f",combo.getDamage()), 10, 30, 0xFFFFFF, true);
 					List<Float> damageList = combo.getDamageList();
 					for (int i = 0; i < damageList.size() && i < Config.getHitsToDisplay(); ++i) {
-						mc.fontRendererObj.drawString(" +" + String.format("%.1f",damageList.get(damageList.size() - i - 1)), 10, 40 + 10 * i, 0xFFFFFF, true);
+						mc.fontRendererObj.drawString(" +" + String.format("%.1f",damageList.get(damageList.size() - i - 1)), 10, 30 + mc.fontRendererObj.FONT_HEIGHT * (i + 1), 0xFFFFFF, true);
 					}
 				}
 				// for Ending Blow, use canUse instead of canExecute to determine whether notification should be displayed
