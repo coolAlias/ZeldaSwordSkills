5789504f71e320f54a031b467e29edd7ab8ecc2e
coolAlias <BASbliznet@gmail.com>
2018-03-29 19:02:17 -0700

    2.6.2-beta build

--- src/main/java/zeldaswordskills/ref/ModInfo.java
+++ src/main/java/zeldaswordskills/ref/ModInfo.java
@@ -20,7 +20,7 @@ package zeldaswordskills.ref;
 public class ModInfo {
 	public static final String ID = "zeldaswordskills";
 	public static final String NAME = "Zelda Sword Skills";
-	public static final String VERSION = "1.7.10-2.6.1-beta";
+	public static final String VERSION = "1.7.10-2.6.2-beta";
 	public static final String CHANNEL = "zss_channel";
 	public static final String CLIENT_PROXY = "zeldaswordskills.ClientProxy";
 	public static final String COMMON_PROXY = "zeldaswordskills.CommonProxy";
